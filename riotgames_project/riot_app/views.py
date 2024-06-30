153e8fc5b517b491984cacffe60d4f597bbb52d7
hyejiining <hyejiining@gmail.com>
2024-06-30 23:14:54 +0900

    6/30

--- riotgames_project/riot_app/views.py
+++ riotgames_project/riot_app/views.py
@@ -24,5 +24,22 @@ def champion_rotation_view(request):
     
     return render(request, 'riot_app/champion_rotation.html', {'champions': champions})
 
+from django.shortcuts import render
+from .utils import get_champion_rotation, get_champion_data, map_champion_id_to_name
+
 def home_view(request):
-    return render(request, 'riot_app/home.html')
+    rotation_data = get_champion_rotation()
+    champion_data = get_champion_data()
+    if rotation_data and champion_data:
+        id_to_name = map_champion_id_to_name(champion_data)
+        champions = []
+        for champion_id in rotation_data['freeChampionIds']:
+            champion_name = id_to_name.get(champion_id)
+            if champion_name:
+                champions.append({
+                    'name': champion_name,
+                    'image': f"http://ddragon.leagueoflegends.com/cdn/11.24.1/img/champion/{champion_name}.png"
+                })
+        return render(request, 'riot_app/home.html', {'champions': champions})
+    return render(request, 'riot_app/home.html', {'champions': []})
+
